c77ba531ec9df8f0c120d5dde219975b28e51c29
Josh <joshuacowan14@gmail.com>
2025-08-07 15:48:07 +0100

    make it so you can only set url via secret, make failed pings not cause errored pods

--- ai-vitals/src/main.rs
+++ ai-vitals/src/main.rs
@@ -24,7 +24,7 @@ async fn main() -> Result<()> {
     let config = Config::parse();
 
     let monitor = Monitor::new(config).context("Failed to create monitor")?;
-    let exit_code = monitor.run().await;
+    let _ = monitor.run().await;
 
-    exit(exit_code);
+    exit(0);
 }
